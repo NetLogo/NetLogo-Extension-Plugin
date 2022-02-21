29c9ea729054728e982691f03a3f321edfdd277b
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2022-02-21 16:34:30 -0600

    Feature: Properly filter out NetLogo transitive deps on package

--- src/main/scala/org/nlogo/build/NetLogoExtension.scala
+++ src/main/scala/org/nlogo/build/NetLogoExtension.scala
@@ -1,8 +1,13 @@
 package org.nlogo.build
 
+import java.io.File
+
 import sbt._, Keys._, plugins.JvmPlugin,
   io.CopyOptions,
   internal.inc.classpath.ClasspathUtilities
+
+import sbt.librarymanagement.{ ModuleID, OrganizationArtifactReport, UpdateReport }
+import sbt.io.NameFilter
 
 import scala.sys.process.Process
 
@@ -61,9 +66,68 @@ object NetLogoExtension extends AutoPlugin {
 
   lazy val netLogoAPIVersion = taskKey[String]("APIVersion of NetLogo associated with compilation target")
 
+  lazy val netLogoDependencies = settingKey[Seq[ModuleID]]("NetLogo libraries and dependencies")
+
+  lazy val crossProjectID = settingKey[ModuleID]("The cross-project ModuleID version of the projectID setting")
+
   import autoImport._
 
   val netLogoPackagedFiles = taskKey[Seq[(File, String)]]("extension-packaged-files")
+
+  // for our simple purposes we only need the organization and name to determine module equality
+  // so we use cases class to handle that for us.   -Jeremy B February 2022
+
+  case class MiniModule(organization: String, name: String)
+  case class CalledModule(module: MiniModule, callers: Set[MiniModule])
+
+  def miniturizeModule(module: ModuleID): MiniModule = MiniModule(module.organization, module.name)
+
+  type RootsMap = Map[MiniModule, Set[MiniModule]]
+
+  def rootsBabyRoots(projectIDs: Set[MiniModule], allModules: Map[MiniModule, CalledModule], rootsSoFar: RootsMap, moduleReport: CalledModule): RootsMap = {
+    // if the rootsSoFar already has this module, we were processed as the caller of a prior module
+    if (rootsSoFar.contains(moduleReport.module)) {
+      rootsSoFar
+    } else {
+      val nonProjectCallers = moduleReport.callers.filter(!projectIDs.contains(_))
+      val callers           = nonProjectCallers.flatMap(allModules.get(_))
+      val newRoots          = callers.foldLeft(rootsSoFar)( (newRoots, callerReport) => rootsBabyRoots(projectIDs, allModules, newRoots, callerReport) )
+      val callerRoots       = callers.map( (callerReport) => {
+        val callerRoots = newRoots.getOrElse(callerReport.module, throw new Exception(s"Did not find a roots entry for $callerReport?"))
+        // if it has no callers, it's the root.
+        if (callerRoots.isEmpty) {
+          Seq(callerReport.module)
+        } else {
+          callerRoots
+        }
+      })
+      val moduleRoots = callerRoots.flatten.toSet
+      newRoots + (moduleReport.module -> moduleRoots)
+    }
+  }
+
+  def createRootsMap(projectIDs: Set[MiniModule], moduleReports: Seq[CalledModule]): RootsMap = {
+    val allModules       = moduleReports.map( (m) => (m.module, m) ).toMap
+    val reportsToRootMap = (rootsSoFar: RootsMap, moduleReport: CalledModule) => rootsBabyRoots(projectIDs, allModules, rootsSoFar, moduleReport)
+    moduleReports.foldLeft(Map[MiniModule, Set[MiniModule]]())(reportsToRootMap)
+  }
+
+  def isNetLogoDependency(miniNetLogoDeps: Set[MiniModule], rootsMap: RootsMap, module: MiniModule): Boolean = {
+    val roots = rootsMap.getOrElse(module, throw new Exception(s"This module does not have a set of roots to inspect?  $module"))
+    miniNetLogoDeps.contains(module) || roots.exists( (r) => miniNetLogoDeps.contains(r) )
+  }
+
+  def getExtensionDependencies(projectIDs: Set[ModuleID], netLogoDependencies: Seq[ModuleID], report: UpdateReport): Seq[File] = {
+    val miniProjectIDs         = projectIDs.map(miniturizeModule).toSet
+    val compileConfiguration   = report.configurations.filter(_.configuration.name == "compile").headOption.getOrElse(throw new Exception("No compile configuration in the project?"))
+    val modules                = compileConfiguration.modules.map( (m) => CalledModule(miniturizeModule(m.module), m.callers.map( (c) => miniturizeModule(c.caller) ).toSet) )
+    val rootsMap               = createRootsMap(miniProjectIDs, modules)
+    val miniNetLogoDeps        = netLogoDependencies.map(miniturizeModule).toSet
+    val extensionModuleReports = compileConfiguration.modules.filter( (m) => !isNetLogoDependency(miniNetLogoDeps, rootsMap, miniturizeModule(m.module)) )
+    extensionModuleReports.flatMap( (moduleReport) =>
+      moduleReport.artifacts.find(_._1.`type` == "jar").orElse(moduleReport.artifacts.find(_._1.extension == "jar")).map(_._2)
+    )
+  }
 
   override lazy val projectSettings = Seq(
 
@@ -74,15 +138,7 @@ object NetLogoExtension extends AutoPlugin {
 
     netLogoZipSources := true,
 
-    netLogoPackageExtras := {
-      (externalDependencyClasspath in Runtime).value.files
-        .filter(path =>
-          path.getName.endsWith(".jar") &&
-          !path.getName.startsWith("scala-library") &&
-          !path.getName.startsWith("NetLogo") &&
-          !path.getName.startsWith("netlogo"))
-        .map(path => (path, path.getName))
-    },
+    netLogoPackageExtras := getExtensionDependencies(Set(projectID.value, crossProjectID.value), netLogoDependencies.value, (Compile / updateFull).value).map( (d) => (d, d.getName) ),
 
     artifactName := ((_, _, _) => s"${netLogoExtName.value}.jar"),
 
@@ -127,48 +183,25 @@ object NetLogoExtension extends AutoPlugin {
     clean := {
       val _ = clean.value
       IO.delete(netLogoTarget.value.producedFiles(netLogoPackagedFiles.value))
-    }
+    },
 
-  ) ++ netLogoJarSettings
+    resolvers += "netlogo" at "https://dl.cloudsmith.io/public/netlogo/netlogo/maven/",
 
-  def netLogoJarSettings: Seq[Setting[_]] = {
+    netLogoDependencies := Seq(
+      "org.nlogo"          %  "netlogo"    % netLogoVersion.value
+    , "org.nlogo"          %  "netlogo"    % netLogoVersion.value % Test classifier "tests"
+    , "org.scalatest"      %% "scalatest"  % "3.2.10" % Test
+    , "org.jogamp.jogl"    %  "jogl-all"   % "2.4.0" from "https://jogamp.org/deployment/archive/rc/v2.4.0-rc-20210111/jar/jogl-all.jar"
+    , "org.jogamp.gluegen" %  "gluegen-rt" % "2.4.0" from "https://jogamp.org/deployment/archive/rc/v2.4.0-rc-20210111/jar/gluegen-rt.jar"
+    ),
 
-    val netLogoJarFile =
-      Option(System.getProperty("netlogo.jar.file"))
-        .map { f =>
-          val jar = file(f)
-          val testJar = file(f.replaceAllLiterally(".jar", "-tests.jar"))
-          Seq(unmanagedJars in Compile ++= Seq(jar, testJar))
-        }
+    libraryDependencies ++= netLogoDependencies.value,
 
-    val netLogoJarURL =
-      Option(System.getProperty("netlogo.jar.url"))
-        .map { url =>
-          val urlVersion = url.split("/").last
-            .stripPrefix("NetLogo")
-            .stripPrefix("-")
-            .stripSuffix(".jar")
-          val version = if (urlVersion == "") "DEV" else urlVersion
-          val testUrl = url.replaceAllLiterally(".jar", "-tests.jar")
-          Seq(libraryDependencies ++= Seq(
-            "org.nlogo" % "NetLogo" % version changing() from url,
-            "org.nlogo" % "NetLogo-tests" % version changing() from testUrl))
-        }
+    crossProjectID := CrossVersion(scalaVersion.value, scalaBinaryVersion.value)(projectID.value)
 
-    (netLogoJarFile orElse netLogoJarURL).getOrElse {
-      Seq(
-        resolvers += "netlogo" at "https://dl.cloudsmith.io/public/netlogo/netlogo/maven/"
-      , libraryDependencies ++= Seq(
-          "org.nlogo"          %  "netlogo"    % netLogoVersion.value
-        , "org.nlogo"          %  "netlogo"    % netLogoVersion.value % Test classifier "tests"
-        , "org.scalatest"      %% "scalatest"  % "3.2.10" % Test
-        , "org.jogamp.jogl"    %  "jogl-all"   % "2.4.0" from "https://jogamp.org/deployment/archive/rc/v2.4.0-rc-20210111/jar/jogl-all.jar"
-        , "org.jogamp.gluegen" %  "gluegen-rt" % "2.4.0" from "https://jogamp.org/deployment/archive/rc/v2.4.0-rc-20210111/jar/gluegen-rt.jar"
-        )
-      )
-    }
-  }
+  )
 
   def directoryTarget(targetDirectory: File): Target =
     new DirectoryTarget(targetDirectory)
+
 }
